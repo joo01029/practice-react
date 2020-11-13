d162e877a128c56f7ebcf71505482a0cdf3987e5
joo01029 <joo01029@naver.com>
2020-11-13 12:01:07 +0900

    use map

--- management/src/App.js
+++ management/src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+ import Customer from './components/Customer';
 
-function App() {
+const customer = [{
+  'id':1,
+  'image':'http://placeimg.com/64/64/any',
+  'name':'홍길동',
+  'birthday':'040404',
+  'gender':'남자',
+  'job':'백수쉑'
+},{
+  'id':2,
+  'image':'http://placeimg.com/64/64/any',
+  'name':'바보',
+  'birthday':'040404',
+  'gender':'여자',
+  'job':'백수쉑'
+},{
+  'id':1,
+  'image':'http://placeimg.com/64/64/any',
+  'name':'멍청이',
+  'birthday':'040404',
+  'gender':'남자',
+  'job':'백수쉑'
+}]
+
+class App extends Component {
+  render(){
     return ( 
-      <div className="gray-background-color">
-        <img src={logo} lat="logo"/>
-        <h2>Hello World!</h2>
+      <div>
+        {
+          customer.map(c=>{
+            return (
+              <Customer
+              key={c.id}
+                id={c.id}
+                image={c.image}
+                name = {c.name}
+                birthday= {c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
       </div>
+      
     );
+  }
 }
+    
+
 
 export default App;
