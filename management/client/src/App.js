d3181a7019c44c834d90a7c02f422c68d57ff4d0
joo01029 <joo01029@naver.com>
2020-11-15 21:08:47 +0900

    circul

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -6,36 +6,49 @@ import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
 import TableHead from '@material-ui/core/TableHead'
 import TableBody from '@material-ui/core/TableBody'
+import Paper from '@material-ui/core/Paper'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import {withStyles} from '@material-ui/core/styles'
 
-const customer = [{
-  'id':1,
-  'image':'http://placeimg.com/64/64/any',
-  'name':'홍길동',
-  'birthday':'040404',
-  'gender':'남자',
-  'job':'백수쉑'
-},{
-  'id':2,
-  'image':'http://placeimg.com/64/64/any',
-  'name':'바보',
-  'birthday':'040404',
-  'gender':'여자',
-  'job':'백수쉑'
-},{
-  'id':1,
-  'image':'http://placeimg.com/64/64/any',
-  'name':'멍청이',
-  'birthday':'040404',
-  'gender':'남자',
-  'job':'백수쉑'
-}]
+const styles = theme => ({
+  root:{
+    width: "100%",
+    marginTop:  theme.spacing(3),
+  },
+  table: {
+    minWidth: 1080
+  },
+  progress:{
+    margin: theme.spacing(2)
+  }
+})
 
 class App extends Component {
+  state = {
+    customers:"",
+    completed: 0
+  }
+  componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res=>this.setState({customers: res}))
+      .catch(err => console.log(err))
+  }
+  callApi = async ()=>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+  progress = ()=> {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0: completed+1});
+    
+  }
   render(){
+    const {classes} = this.props;
     return ( 
-      <div>
-        <Table>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
               <TableCell>번호</TableCell>
@@ -46,14 +59,21 @@ class App extends Component {
               <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>{customer.map(c=>{return(<Customer key={c.id} id={c.id} name={c.name} image={c.image} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c=>{return(<Customer key={c.id} id={c.id} name={c.name} image={c.image} birthday={c.birthday} gender={c.gender} job={c.job}/>)}):
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow> 
+            }
           </TableBody>
         </Table>
-      </div>
+      </Paper>
     );
   }
 }
     
 
 
-export default App;
+export default withStyles(styles)(App);
--- management/server.js
+++ management/server.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const app = express()
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/api/hello', (req, res)=>{
-    res.send({message:"hello"})
+app.get('/api/customers', (req,res)=>{
+    res.send([{
+        'id':1,
+        'image':'http://placeimg.com/64/64/any',
+        'name':'홍길동',
+        'birthday':'040404',
+        'gender':'남자',
+        'job':'백수쉑'
+      },{
+        'id':2,
+        'image':'http://placeimg.com/64/64/any',
+        'name':'바보',
+        'birthday':'040404',
+        'gender':'여자',
+        'job':'백수쉑'
+      },{
+        'id':1,
+        'image':'http://placeimg.com/64/64/any',
+        'name':'멍청이',
+        'birthday':'040404',
+        'gender':'남자',
+        'job':'백수쉑'
+      }])
 })
-
 
 app.listen(port, ()=>{
     console.log('server start');
