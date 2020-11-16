a92e68c908fd3d205f9bb2af8020e5ec58c9ec28
joo01029 <joo01029@naver.com>
2020-11-16 20:16:03 +0900

    customer add

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
-import Table from '@material-ui/core/Table';
+import CustomerAdd from './components/customerAdd';
+
+//css
+import Table from '@material-ui/core/Table'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
 import TableHead from '@material-ui/core/TableHead'
@@ -14,6 +17,7 @@ const styles = theme => ({
   root:{
     width: "100%",
     marginTop:  theme.spacing(3),
+    overflowX: 'auto'
   },
   table: {
     minWidth: 1080
@@ -24,10 +28,24 @@ const styles = theme => ({
 })
 
 class App extends Component {
-  state = {
-    customers:"",
-    completed: 0
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
   }
+  stateRefresh = ()=>{
+    this.setState({
+      customers:'',
+      completed: 0
+    });
+    this.callApi()
+      .then(res=>this.setState({customers: res}))
+      .catch(err => console.log(err))
+  }
+
   componentDidMount(){
     this.timer = setInterval(this.progress, 20);
     this.callApi()
@@ -47,29 +65,33 @@ class App extends Component {
   render(){
     const {classes} = this.props;
     return ( 
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>사진</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c=>{return(<Customer key={c.id} id={c.id} name={c.name} image={c.image} birthday={c.birthday} gender={c.gender} job={c.job}/>)}):
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-              </TableCell>
-            </TableRow> 
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>사진</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map(c=>{return(<Customer key={c.id} id={c.id} name={c.name} image={c.image} birthday={c.birthday} gender={c.gender} job={c.job}/>)}):
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow> 
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
+      
+      </div>
     );
   }
 }
--- management/client/src/components/Customer.js
+++ management/client/src/components/Customer.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+
+
 class Customer extends React.Component {
     render(){
+        const styles = theme => ({
+            image:{
+                width:"64px",
+                heigth:"64px"
+            }
+          })
         return (
            <TableRow>
                <TableCell>{this.props.id}</TableCell>
-               <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+               <TableCell><img src={this.props.image} alt="profile" className={styles.image}/></TableCell>
                <TableCell>{this.props.name}</TableCell>
                <TableCell>{this.props.birthday}</TableCell>
                <TableCell>{this.props.gender}</TableCell>
